0c86f9b9c61c53842d42beb2f0ea7ab30aa2b58d
sitiritis <tymur.lysenko@gmail.com>
2020-03-08 20:26:55 +0300

    Sparse test and report

---
+++ assignment_1/test/prolog/15-sparse.pl
@@ -0,0 +1,13 @@
+% Big map, with 1 orc, some humans and touchdown point almost in the corner
+
+test_name('15-sparse').
+
+minX(0).
+maxX(9).
+minY(0).
+maxY(9).
+
+h(4, 3).
+h(3, 2).
+o(1, 2).
+t(8, 9).
