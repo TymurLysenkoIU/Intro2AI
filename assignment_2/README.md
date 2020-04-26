cdb1b79ede9b6da178694632b23f75f7722dc950
Tymur Lysenko <tymur.lysenko@gmail.com>
2020-04-26 23:52:09 +0300

    Generate random polygons inscribed into a circle

---
+++ assignment_2/README.md
@@ -0,0 +1,13 @@
+# Project structure
+
+- `requirements.txt` - a file with python dependencies. To install all the necessary dependencies, run
+
+```shell script
+pip install -r requirements.txt
+```
+
+- `notebook.ipynb` - jupyter notebook with the source code for the assignment
+
+- `conf.py` - config file, containing values used by the algorithm
+
+- `img/` - folder containing source images
---
+++ assignment_2/conf.py
@@ -0,0 +1,13 @@
+# Path to the image to be processed
+IMAGE_BASE_PATH = './img/'
+IMAGE_NAME = 'ricado_1.jpg'
+IMAGE_PATH = IMAGE_BASE_PATH + IMAGE_NAME
+
+# Visual parameters
+# Number of vertices for polygons, which are composed into an image; half-open interval
+MIN_NUM_POLYGON_VERTICES = 3   # should be >= 3
+MAX_NUM_POLYGON_VERTICES = 17
+
+# Sizes of the circumscribed circle around a polygon; half-open interval
+MIN_CIRCUMSCRIBED_CIRCLE_SIZE = 5   # px
+MAX_CIRCUMSCRIBED_CIRCLE_SIZE = 31  # px
